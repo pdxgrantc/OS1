ea350f9d3e7685e4685594623ff4fa0ba598b458
Grant Conklin <conkling@flip1.engr.oregonstate.edu>
2022-06-29 12:19:30 -0700

    created terminal directory

--- data_structures/main.c
+++ data_structures/main.c
@@ -3,12 +3,11 @@
 #include "linked_list.h"
 #include "dynamic_ary.h"
 
-void linked_list_driver() {
+void linked_list_driver()
+{
 	printf("Linked List Driver\n");
 	struct List *list = new_list();
-
 	int i;
-
 	for (i = 0; i < 4; i++)
 	{
 		push_front_list(list, i);
@@ -17,12 +16,11 @@ void linked_list_driver() {
 	delete_list(list);
 }
 
-void dynamic_ary_driver() {
+void dynamic_ary_driver()
+{
 	printf("Dynamic Array Driver\n");
 	struct Ary *ary = new_ary();
-
 	int i;
-
 	for (i = 0; i < 5; i++)
 	{
 		int *x = malloc(sizeof(int));
@@ -31,17 +29,17 @@ void dynamic_ary_driver() {
 	}
 	for (size_t i = 0; i < get_length_ary(ary); i++)
 	{
-		printf("%d\n", *(int*)get_item_ary(ary, i));
+		printf("%d\n", *(int *)get_item_ary(ary, i));
 		free(get_item_ary(ary, i));
 	}
-	
+
 	delete_ary(ary);
 }
 
 int main()
 {
 	printf("Hello, World!\n");
-	//linked_list_driver();
+	// linked_list_driver();
 	dynamic_ary_driver();
 	return 0;
 }
